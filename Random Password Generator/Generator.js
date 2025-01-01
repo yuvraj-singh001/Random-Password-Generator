5aaf0062256aface34dbba139ca1df76e87e6244
Yuvraj Singh <yuvrajkypn@gmail.com>
2025-01-01 11:58:33 +0530

    Adding Files

---
+++ Random Password Generator/Generator.js
@@ -0,0 +1,34 @@
+const passwordbox = document.querySelector(".password");
+const generatebtn = document.querySelector(".generatebtn");
+const copybtn = document.querySelector(".copybtn");
+const length = 12;
+
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+`~{}[]|\/-=";
+
+const allchars = uppercase + lowercase + number + symbol;
+
+function createpassword(){
+    let password = "";
+    // password += uppercase[Math.floor(Math.random()*uppercase.length)];
+    // password += lowercase[Math.floor(Math.random()*lowercase.length)];
+    // password += number[Math.floor(Math.random()*number.length)];
+    // password += symbol[Math.floor(Math.random()*symbol.length)];
+
+    while(length>password.length){
+        password += allchars[Math.floor(Math.random()*allchars.length)]
+    }
+
+    passwordbox.value = password;
+}
+
+function copypassword(){
+    passwordbox.select();
+    document.execCommand("copy");
+}
+
+copybtn.addEventListener("click",copypassword);
+generatebtn.addEventListener("click",createpassword);
